eef5330c88cf2ac9245950d8dafa9f8047520243
Victor <victorrivera714@yahoo.com>
2021-06-10 15:04:07 -0700

    commit tree 3e49665d805df317e716dd3dd8bd6f6f2e03794d

--- index.js
+++ index.js
@@ -1,2 +1,22 @@
+let multiply
 
-// Write your code here
+let num1 = 31; 
+let num2 = 2;
+multiply= num1 * num2;
+
+
+let random
+// Math.floor(Math.random() * 10) + 1;
+random = Math.floor(Math.random() * 10) + 1;
+
+
+
+
+let mod
+let num3 = 32;
+let num4 = 7;
+mod = num3 % num4;
+
+
+let max
+max = Math.max(1, 2, 3, 4, 20);
